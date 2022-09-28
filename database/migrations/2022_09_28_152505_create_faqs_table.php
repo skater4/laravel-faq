08d27dad12529e083066964145b2078ca9c09365
Stan <manager@cs-coding.com>
2022-09-28 23:38:51 +0800

    init

---
+++ database/migrations/2022_09_28_152505_create_faqs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFaqsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faqs', function (Blueprint $table) {
+            $table->id('message_id');
+            $table->integer('thread_id');
+            $table->string('name');
+            $table->string('user_type');
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faqs');
+    }
+}
